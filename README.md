7f066552e02ed027b9b9666796b4fbf967b1887b
xero-lib <jonathankurtz@protonmail.com>
2023-11-28 19:40:23 -0600

    v0.1 complete

---
+++ README.md
@@ -0,0 +1,2 @@
+<h1 align="center">Preview Delete</h1>
+<em align="center">Preview delete is a small shell script that allows you to preview a list of files and decide what you want to do with each</em>
---
+++ preview_delete.sh
@@ -0,0 +1,33 @@
+#!/bin/env bash
+echo -n "What program should be used to preview the files? "
+read prog
+
+if ! command -v $prog &> /dev/null 
+then
+	echo "Unable to find command \"$prog\""
+	exit 1
+fi
+
+for file in "$@"
+do
+	$prog "$file"
+	read -p "Remove this file? [y/N] " rmyn
+	case $rmyn in
+		[Yy]* ) rm -vf "$file";;
+		[Cc]* ) exit;;
+		* )
+			read -p "Would you like to rename this file? [y/N] " mvyn
+			case $mvyn in
+				[Yy]* )
+					read -p "Destination name: " name
+					while [ -f "$name" ] || [ "$name" = "" ]; do
+						read -p "This name is invalid or already in use. Please enter another name: " name;
+					done
+					mv $file $name
+				;;
+				* ) echo "Retaining original name" ;;
+			esac
+		;;
+	esac
+done
+		
